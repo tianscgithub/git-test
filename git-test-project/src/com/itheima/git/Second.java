2d06d1a6b8f0faaa67acc1911cd52d3c3ce83a5f
tiansc <18225510580@sina.cn>
2019-12-31 13:05:59 +0800

    添加一个second类

---
+++ git-test-project/src/com/itheima/git/Second.java
@@ -0,0 +1,13 @@
+package com.itheima.git;
+
+/**
+ * @author tiansc
+ * @description TODO
+ * @date Created in 13:04 2019/12/31
+ * @modified by
+ */
+public class Second {
+    public void  test() {
+        System.out.println("hello world");
+    }
+}
