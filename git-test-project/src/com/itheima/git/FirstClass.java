e43f6d775f8a0bfb3d476b46f3db20405a2902d6
tiansc <18225510580@sina.cn>
2019-12-31 10:48:32 +0800

    自己建立的项目git提交

---
+++ git-test-project/src/com/itheima/git/FirstClass.java
@@ -0,0 +1,16 @@
+package com.itheima.git;
+
+/**
+ * @author tiansc
+ * @description TODO
+ * @date Created in 10:11 2019/12/31
+ * @modified by
+ */
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("1234");
+    }
+}
