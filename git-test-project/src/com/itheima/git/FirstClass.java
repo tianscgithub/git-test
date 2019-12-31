3ecb1642a02f135756343d513774b7649af8488e
tiansc <18225510580@sina.cn>
2019-12-31 12:58:49 +0800

    tijiao

--- git-test-project/src/com/itheima/git/FirstClass.java
+++ git-test-project/src/com/itheima/git/FirstClass.java
@@ -12,11 +12,13 @@ public class FirstClass {
         System.out.println("hello git project");
         System.out.println("hello git project");
         System.out.println("1234");
+        //hello test
     }
 
     public void test () {
         int i = 0;
         i += 10;
+        System.out.println(i);
     }
 }
 
